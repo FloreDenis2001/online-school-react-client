bccb11057988eca133d407b7640596d8ac1ee521
FloreDenis2001 <floredenis907@yahoo.com>
2023-04-20 20:37:10 +0300

    last_update

---
+++ src/components/Home.tsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+
+function Home() {
+  return (
+    <body>
+
+<Header/>
+
+    <main>
+        <div className="all-learnings">
+            <div className="book id-1">
+                <img src="/img/library2.jpeg" alt=""/>
+                <p className="describe">Splendid Woodland-gilia</p>
+                <p className="price">23.00 RON</p>
+                <button className="go">Read</button>
+            </div>
+            <div className="book id-1">
+                <img src="/img/library2.jpeg" alt=""/>
+                <p className="describe">Splendid Woodland-gilia</p>
+                <p className="price">23.00 RON</p>
+                <button className="go">Read</button>
+            </div>
+            <div className="book id-1">
+                <img src="/img/library2.jpeg" alt=""/>
+                <p className="describe">Splendid Woodland-gilia</p>
+                <p className="price">23.00 RON</p>
+                <button className="go">Read</button>
+            </div>
+            <div className="book id-1">
+                <img src="/img/library2.jpeg" alt=""/>
+                <p className="describe">Splendid Woodland-gilia</p>
+                <p className="price">23.00 RON</p>
+                <button className="go">Read</button>
+            </div>
+            <div className="book id-1">
+                <img src="/img/library2.jpeg" alt=""/>
+                <p className="describe">Splendid Woodland-gilia</p>
+                <p className="price">23.00 RON</p>
+                <button className="go">Read</button>
+            </div>
+            <div className="book id-1">
+                <img src="/img/library2.jpeg" alt=""/>
+                <p className="describe">Splendid Woodland-gilia</p>
+                <p className="price">23.00 RON</p>
+                <button className="go">Read</button>
+            </div>
+            <div className="book id-1">
+                <img src="/img/library2.jpeg" alt=""/>
+                <p className="describe">Splendid Woodland-gilia</p>
+                <p className="price">23.00 RON</p>
+                <button className="go">Read</button>
+            </div>
+            <div className="book id-1">
+                <img src="/img/library2.jpeg" alt=""/>
+                <p className="describe">Splendid Woodland-gilia</p>
+                <p className="price">23.00 RON</p>
+                <button className="go">Read</button>
+            </div>
+        </div>
+    </main>
+
+<Footer/>
+
+</body>
+  )
+}
+
+export default Home
---
+++ src/models/Book.ts
@@ -0,0 +1,8 @@
+export default interface Book {
+    id:number,
+    author:string,
+    price:number,
+    stars:number,
+    title:string,
+    user_id:number
+}
